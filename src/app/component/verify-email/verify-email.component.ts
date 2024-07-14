29b6d760a232a391def5dd844675942858b3420a
atul18singh <atul18singh@gmail.com>
2024-07-14 17:50:26 +0530

    veryfiy email

--- src/app/component/verify-email/verify-email.component.ts
+++ src/app/component/verify-email/verify-email.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verify-email',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerifyEmailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  back(){
+    this.router.navigate(['/login'])
+  }
 }
