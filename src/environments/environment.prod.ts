265113ac08261fac363f26582dea6772c2c8edae
atul18singh <atul18singh@gmail.com>
2024-07-17 20:20:03 +0530

    fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBXvSJXJ1KzJ30l7w-x9oFfsBgLo-V6Kl0",
+    authDomain: "dairy-management-system-ffc7c.firebaseapp.com",
+    projectId: "dairy-management-system-ffc7c",
+    storageBucket: "dairy-management-system-ffc7c.appspot.com",
+    messagingSenderId: "278834763847",
+    appId: "1:278834763847:web:cebd8dcbb111198f9c7962"
+  }
 };
